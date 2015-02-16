a0e57273419e2e63f8d447cfd0e54e8c9c591878
Charles Shinaver <bshinaver@gmail.com>
2015-02-15 20:46:49 -0500

    Added development settings class

---
+++ VoiceRecorder/DevelopmentSettings.m
@@ -0,0 +1,13 @@
+//
+//  DevelopmentSettings.m
+//  VoiceRecorder
+//
+//  Created by Charles Shinaver on 2/15/15.
+//  Copyright (c) 2015 University of Notre Dame. All rights reserved.
+//
+
+#import "DevelopmentSettings.h"
+
+@implementation DevelopmentSettings
+
+@end
